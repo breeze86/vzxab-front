94a44604d42677546b84fbf866d56bfbe5743fa5
breeze <yangwf86@gmail.com>
2025-12-27 14:23:52 +0800

    Add: pm2.config.js

---
+++ pm2.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: 'coolflow-portal',
+      script: '/usr/local/bin/pnpm',
+      args: 'start',
+      cwd: __dirname,
+      autorestart: true,
+      merge_logs: true,
+      max_memory_restart: '2G',
+      env: {
+        ENV_PATH: '.env'
+      }
+    }
+  ]
+}
